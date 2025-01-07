c5195e49c138becc53ccbeb6d86189ff048ce269
AumThaker <thakeraum1511@gmail.com>
2025-01-07 19:24:01 +0530

    commit

--- movieApi/API.js
+++ movieApi/API.js
@@ -99,6 +99,11 @@ const homeMovieDisplay = {
             movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
             movieLength: "2h 4m",
         },
+        "20":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+        },
 }
 const movies = 
     {
@@ -129,6 +134,240 @@ const movies =
             movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
         },
         "2":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "3":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "4":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "5":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "6":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "7":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "8":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "9":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "10":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "11":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "12":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "13":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "14":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "15":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "16":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "17":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "18":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "19":{
+            movieName: "Iron Man 2",
+            movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
+            movieLength: "2h 4m",
+            movieLink:"https://dl.dropboxusercontent.com/scl/fi/ihuse9puxwxm17xe6b8cx/Iron_Man_2_-2010-_Hindi_Dubbed.mp4?rlkey=k0kp1m28jpbs9xq2bji2mnlyo&st=nvjvyw0p&dl=0",
+            movieReleaseYear: "2010",
+            movieRatings:{
+                "imDB": "6.9",
+                "rottenTomatoes": "72%"
+            },
+            movieTrailer: "",
+            movieDesc: "Tony Stark is under pressure from various sources, including the government, to share his technology with the world. He must find a way to fight them while also tackling his other enemies."
+        },
+        "20":{
             movieName: "Iron Man 2",
             movieImage: "https://res.cloudinary.com/dplo3ynm6/image/upload/v1733818315/oyv1fzcktjourkfmwqxp.jpg",
             movieLength: "2h 4m",
